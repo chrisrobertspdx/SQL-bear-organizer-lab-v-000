337cf1e1d4a24265b8523826fd837d6673d97e0d
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-11 20:03:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "select count(color) as thecount,color from bears group by color order by thecount desc limit 1"
+  "select color,count(color) as thecount from bears group by color order by thecount desc limit 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
