096407d6adb0b729953bb861bcdad961d9d14711
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-11 20:02:44 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "select count(color),color from bears group by color"
+  "select count(color) as thecount,color from bears group by color order by thecount desc limit 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "select * from bears where name = NULL"
+  "select * from bears where name = nil"
 end
