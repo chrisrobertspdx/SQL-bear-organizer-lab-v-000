8b8b0390d4abeca579b33c98bdfd6b016c3726b7
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-11 19:38:53 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  gender TEXT,
+  gender CHAR(1),
   age INTEGER,
   color TEXT,
   temperament TEXT,
-  alive TEXT
+  alive BOOLEAN
 );
